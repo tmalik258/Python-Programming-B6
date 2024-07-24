cd19f9f83b17430c85d8b91044cde6c67ba5b8f4
tmalik258 <talhamalik25.tm@gmail.com>
2024-07-24 20:54:41 +0500

    Datatypes, variables

---
+++ JavaScript/Day 2/variables.js
@@ -0,0 +1,56 @@
+var r = 74; // int
+const PIE = 3.1416; // float
+
+var result; // declaring the variable named as result
+
+result = PIE * r;
+
+console.log(result);
+
+console.log(PIE);
+console.log(r);
+
+
+r = 12; // assignment operator
+
+result = PIE * r ** 2;  // ** means exponentiation or exponent
+console.log(r);
+console.log("Area of Triangle", " = ", result); // print to console
+
+
+// area of rectangle
+
+// area of triangle
+
+// variable names can only start by "alphabet", "$", "_"
+
+// javascript is case sensitive
+
+
+var helo = 'this is me, hello'; // string
+let hekdj = 'helo this is me'; // string
+
+document.write('<h1>Area of Triangle = </h1>', result);
+
+// alert('helllo world!', result);
+
+
+// Arithmetic Operators - / * + - ** -- ++ %(mod short for modulus)
+
+let z = 12;
+
+z = z + 1;
+
+console.log(z);
+
+z++; // increment by 1
+
+console.log(z);
+
+z--; // decrement by 1
+
+console.log(z);
+
+z = z * 2;
+
+let y = 5 % 2;
