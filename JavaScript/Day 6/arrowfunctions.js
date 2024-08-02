4e4c1da09afa7c8aed8ecc475cb830e1fd4f0adc
tmalik258 <talhamalik25.tm@gmail.com>
2024-08-02 20:59:28 +0500

    revision

--- JavaScript/Day 6/arrowfunctions.js
+++ JavaScript/Day 6/arrowfunctions.js
@@ -1,7 +1,7 @@
-const sum = (a, b) => {
-    let result = a + b;
-    console.log(result);
-} // anonymous function or benaam function
+const sum = (a, b) => console.log("sum: ", a + b);
+
+
+// anonymous function or benaam function
 
 sum(12, 34);
 
@@ -9,3 +9,17 @@ sum(12, 34);
 // logical operator
 // comparison operator
 
+
+// two parameter arrow function
+// const avg = (a, b) => {
+//     let result = (a + b)/2;
+//     console.log(a, b);
+// }
+const avg = (a, b) => console.log("avg: ", (a + b)/2);
+
+avg(23, 58);
+
+// one parameter arrow function can be with parenthesis or withou parenthesis
+const greeting = name => console.log("Hi ", name, "!");
+
+greeting("talha");
--- JavaScript/Day 6/functions.js
+++ JavaScript/Day 6/functions.js
@@ -34,11 +34,37 @@ greater(12, 15) // two parameters functions
 greater(58, 1);
 greater(85, 23);
 
-function sum (a, b) {
-    let result = a + b;
-    console.log(result);
-}
 
 greater(5, 2);
 
-sum(2585, 1)
+let result = 0;
+
+// function sum (a, b) {
+//     return result;
+// }
+
+console.log(result);
+
+result = sum(2, 1);
+console.log(result);
+console.log(sum(2, 5));
+
+// if a is greater than b, then return sum of a and b
+// else return product of a and b
+
+// Write a program that takes in a string that has camel casing and outputs
+// the same string but with snake casing
+
+// sample input:        "camelCasing"
+// sample output:       "camel_casing"
+
+
+let isResultToName = "camelCasing";
+let is_result_to = 'snake_casing';
+
+
+let input = "talhaMalik";
+
+
+// input = prompt("give me a string");
+
