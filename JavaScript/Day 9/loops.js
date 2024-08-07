b54db0b26b27b1c482edde7a3989c7d89efd6063
tmalik258 <talhamalik25.tm@gmail.com>
2024-08-07 20:52:18 +0500

    For and for in loop

---
+++ JavaScript/Day 9/loops.js
@@ -0,0 +1,48 @@
+// for (let i=1; i < 5; i = i + 1) {
+//     console.log('hello world!');
+// }
+
+for (let i=1; i<20; i=i*2) {
+    console.log(i);
+}
+
+// curly braces
+let arrays = [23, 67, 90, 'hello', 'hi'];
+
+
+// parenthesis
+for (let i=0; i < 5; i++) {
+    console.log(arrays[i]);
+}
+
+let test = {
+    name: "Azad bhai",
+    age: 85,
+    gender: "Male"
+}
+
+// for in loop
+for (let key in test) {
+    console.log(key, test[key]);
+}
+
+
+// console.log(test['age'])
+
+
+let obj = {
+    firstName: "Talha",
+    lastName: "Malik",
+    age: 23,
+    occupation: "Software Eng."
+}
+
+let test1 = 0;
+for (let key in obj) {
+    // console.log(test1);
+    test1++;
+}
+
+// concatenation
+// dont use console.log inside for loop, instead use outside of loop
+// "TalhaMalik23Software Eng."
