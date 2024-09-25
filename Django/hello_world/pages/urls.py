00709d7599d090d77abfdb305ba949c0598ae58e
tmalik258 <talhamalik25.tm@gmail.com>
2024-09-25 19:19:07 +0500

    layouts, url names

--- Django/hello_world/pages/urls.py
+++ Django/hello_world/pages/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from pages.views import HomeView, aboutView
 
 urlpatterns = [
- path('', HomeView.as_view()),
- path('about/', aboutView),
+ path('', HomeView.as_view(), name='index'),
+ path('about/', aboutView, name='about'),
 ]
