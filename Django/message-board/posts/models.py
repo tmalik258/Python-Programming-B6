475302394a32bffa24baf6bbf2b61e02b65f5a51
tmalik258 <talhamalik25.tm@gmail.com>
2024-09-27 21:02:19 +0500

    primary key, foreign key

--- Django/message-board/posts/models.py
+++ Django/message-board/posts/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
+    title = models.TextField(max_length=500)
     body = models.TextField(max_length=500)
 
     def __str__(self):
-        return self.body[:50]
+        return self.body
