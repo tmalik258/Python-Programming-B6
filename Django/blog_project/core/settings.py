0646c81fc277eaff62344a8afcfaea068c201298
tmalik258 <talhamalik25.tm@gmail.com>
2024-11-11 19:24:24 +0500

    .env added

--- Django/blog_project/core/settings.py
+++ Django/blog_project/core/settings.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # take environment variables from .env.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-68b+@(r7hfs74%2cl*dqanif^)z46_u088kp+ny#1mf6s$4lso'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
